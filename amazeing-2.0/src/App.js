8901573aa8719d56b4d5de82c1c89a5d7e5251ae
Monique-K <mkhoury87@hotmail.com>
2019-01-22 15:41:32 -0500

    rendering maze

--- amazeing-2.0/src/App.js
+++ amazeing-2.0/src/App.js
@@ -14,11 +14,55 @@ class App extends Component {
     }
   }
 
+  componentWillMount = () => {
+    this.setState({
+      currentMaze: maze1,
+      mazeX: mazeInfo.width,
+      mazeY: mazeInfo.height
+    })
+  }
+
+  // setMazeStyle = () => {
+  //   if (this.state.mazeX && this.state.mazeY) {
+  //     const mazeStyle = {
+  //       width: this.state.width * 10, 
+  //       height: this.state.height * 10
+  //     }
+  //     return mazeStyle
+  //   }
+  // }
+
+  renderMaze = () => {
+    const mazeArray = []
+    // if (this.state.currentMaze !== null) {
+      let maze = this.state.currentMaze
+      for (let row = 0; row < maze.length; row ++) {
+        for (let col = 0; col < maze[row].length; col++) {
+          if (maze[row][col] === 1) {
+            mazeArray.push(<div id={`row${row}-col${col}`} className='wall'></div>);
+          } else if (maze[row][col] === 0) {
+            mazeArray.push(<div id={`row${row}-col${col}`} className='ground'></div>);
+          } else if (maze[row][col] === 3) {
+            mazeArray.push(<div id='player' className='player'></div>);
+            // mazeArray.push(img id='player' src='/imgs/t-rex.jpeg' className='player'></div>);
+          } else if (maze[row][col] === 4) {
+            mazeArray.push(<div id={`row${row}-col${col}`} src='/imgs/coin.jpg' className='coin'></div>);
+          }
+        }
+      }
+    // }
+    return mazeArray
+  }
+  
+
   render() {
     return (
       <div className="App">
+      
+        <div className="title-container">a-MAZE-ing</div>
         
-        <div className="maze-container" style={{width: this.state.width, height: this.state.height}}>
+        <div className="maze-container">
+        {this.renderMaze()}
         </div>
 
       </div>
@@ -27,3 +71,12 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+TO DO:
+
+-set maze container size based on maze
+
+
+
+*/

62e3afe893ecaea6ca1e75a2293f2b8a5fb7da58
Monique-K <mkhoury87@hotmail.com>
2019-01-24 16:56:18 -0500

    Added buttons to start each maze

--- amazeing-2.0/src/App.js
+++ amazeing-2.0/src/App.js
@@ -4,6 +4,8 @@ import { maze1Info } from './mazes/maze1';
 import { maze1 } from './mazes/maze1';
 import { maze2Info } from './mazes/maze2';
 import { maze2 } from './mazes/maze2';
+import { maze3Info } from './mazes/maze3';
+import { maze3 } from './mazes/maze3';
 
 class App extends Component {
   constructor(){
@@ -11,29 +13,21 @@ class App extends Component {
 
     this.state = {
       win: false,
-      currentMaze: maze2,
-      mazeX: maze2Info.width,
-      mazeY: maze2Info.height,
-      boxSize: maze2Info.boxSize,
+      currentMaze: maze1,
+      mazeX: maze1Info.width,
+      mazeY: maze1Info.height,
+      boxSize: maze1Info.boxSize,
       score: 0,
       winningPos: {
-        row: maze2Info.winningPos.row,
-        col: maze2Info.winningPos.col
+        row: maze1Info.winningPos.row,
+        col: maze1Info.winningPos.col
       },
       player: {
-        row: maze2Info.player.row,
-        col: maze2Info.player.col
+        row: maze1Info.player.row,
+        col: maze1Info.player.col
       },
     }
   }
-
-  // componentWillMount = () => {
-  //   this.setState({
-  //     currentMaze: maze1,
-  //     mazeX: mazeInfo.width,
-  //     mazeY: mazeInfo.height
-  //   })
-  // }
 
   componentDidMount = () => {
     document.addEventListener("keydown", this.movePlayer)
@@ -146,7 +140,7 @@ class App extends Component {
   }
     
 
-  replay = () => {
+  playOne = () => {
     this.setState({
       win: false,
       currentMaze: maze1,
@@ -164,6 +158,44 @@ class App extends Component {
       }
     })
   }
+
+  playTwo = () => {
+    this.setState({
+      win: false,
+      currentMaze: maze3,
+      mazeX: maze3Info.width,
+      mazeY: maze3Info.height,
+      boxSize: maze3Info.boxSize,
+      score: 0,
+      winningPos: {
+        row: maze3Info.winningPos.row,
+        col: maze3Info.winningPos.col
+      },
+      player: {
+        row: maze3Info.player.row,
+        col: maze3Info.player.col
+      }
+    })
+  }
+
+  playThree = () => {
+    this.setState({
+      win: false,
+      currentMaze: maze2,
+      mazeX: maze2Info.width,
+      mazeY: maze2Info.height,
+      boxSize: maze2Info.boxSize,
+      score: 0,
+      winningPos: {
+        row: maze2Info.winningPos.row,
+        col: maze2Info.winningPos.col
+      },
+      player: {
+        row: maze2Info.player.row,
+        col: maze2Info.player.col
+      }
+    })
+  }
   
 
   render() {
@@ -171,22 +203,33 @@ class App extends Component {
 
     return (
       <div className="App">
-      
         <div className="title-container">a-MAZE-ing</div>
-        
-        <div className="maze-container" style={{height: this.state.mazeY * px, width: this.state.mazeX * px}}>
-          {this.renderMaze()}
-          {this.showPlayer()}
-        </div>
-        <div className="bottom-container">
-          <div className="score">
-            Score: {this.state.score}
+        <div className="main">
+          <div className="section-1">
+            <div className="maze-container" style={{height: this.state.mazeY * px, width: this.state.mazeX * px}}>
+              {this.renderMaze()}
+              {this.showPlayer()}
+            </div>
+            <div className="bottom-container">
+              <div className="score">
+                Score: {this.state.score}
+              </div>
+            </div>
           </div>
-          <div className="replay-btn-container">
-            <div className="replay-btn" onClick={this.replay}>Play Again</div>
+          <div className="section-2">
+            <div className="btn-container">
+              <div className="btn maze1-btn" onClick={this.playOne}>
+                Level 1
+              </div>
+              <div className="btn maze2-btn" onClick={this.playTwo}>
+                Level 2
+              </div>
+              <div className="btn maze3-btn" onClick={this.playThree}>
+                Level 3
+              </div>
+            </div>
           </div>
         </div>
-
       </div>
     );
   }
@@ -197,11 +240,10 @@ export default App;
 /*
 TO DO:
 
-- set maze container size based on maze
-- change replay fn to account for which maze is current
 - fix diseappearing treasure on replay
 - add timing to score
 - add name and leaderboard, persistent score
+- add difficulty meter to level buttons
 
 
 
